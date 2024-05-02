bef1578eb6ffc38e75e8a7235a543f49f3f19023
Montassar <jemaamontasar@gmail.com>
2024-05-02 23:58:03 +0100

    Initial commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+<!-- Log on to codeastro.com for more projects! -->
+     ©  {{date('Y')}} Employee Management System <span class="d-none d-sm-inline-block"> - Crafted with <i class="mdi mdi-heart text-danger"></i> by JEMAA Montassar</span>
+</footer>
